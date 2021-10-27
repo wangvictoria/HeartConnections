6a41f888730643fa9aeb140689f101ab531392dd
Victoria Wang <victoriawang29@gmail.com>
2021-10-27 18:47:27 -0500

    Changed homepage

--- LoveAajKalApp/urls.py
+++ LoveAajKalApp/urls.py
@@ -5,7 +5,9 @@ from .views import CreateProfile
 urlpatterns = [
     path('', views.index, name='index'),
     path('profile/create/', views.CreateProfile, name='create_profile'),
-    path('profile/view/', views.ProfileDetailedView, name='profile_detailed_view')
+    path('profile/view/', views.ProfileDetailedView, name='profile_detailed_view'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     #path('profile/<int:pk>/', ProfileUpdateView.as_view(), name='profile_update'),
     #path('profile/<int:pk>/delete/', ProfileDeleteView.as_view(), name='profile_delete'),
 ]
