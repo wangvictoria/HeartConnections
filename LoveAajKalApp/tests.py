1c70d2c641df420be7c1ad74c4c03da3c2bd77de
SovannChang <sovann.d.chang@vanderbilt.edu>
2021-11-05 01:13:27 -0500

    Added tests

--- LoveAajKalApp/tests.py
+++ LoveAajKalApp/tests.py
@@ -9,15 +9,17 @@ Homework #3
 
 from django.test import TestCase
 from .models import Profile as ProfileModel
+from .forms import ProfileForm
 
 # This is the video that shows unit tests in Django:
 # https://www.youtube.com/watch?v=DmRpNoQEx2o&ab_channel=GoDjango 
 
 # Create your tests here.
-class ProfileTest(TestCase):   
-    def test_createProfile(self):
+class ProfileTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
         # Create a profile
-        print("CREATE PROFILE")
         profile = ProfileModel()
 
         profile.first_name = "John"
@@ -40,48 +42,25 @@ class ProfileTest(TestCase):
         profile.email = "johndoe@vanderbilt.edu"
         profile.save()
 
-        record = ProfileModel.objects.get(pk=1)
-        self.assertEqual(record, profile)
+    def test_first_name(self):
+        profile = ProfileModel.objects.get(pk=1)
+        first_name = profile._meta.get_field('first_name').verbose_name
+        self.assertEqual(first_name, 'first name')
 
-    def test_createInvalidProfile(self):
-        # Create a profile
-        print("CREATE INVALID PROFILE")
-        profile = ProfileModel()
+    def test_last_name(self):
+        profile = ProfileModel.objects.get(pk=1)
+        last_name = profile._meta.get_field('last_name').verbose_name
+        self.assertEqual(last_name, 'last name')
+    
+    def test_age(self):
+        profile = ProfileModel.objects.get(pk=1)
+        age = profile._meta.get_field('age').verbose_name
+        self.assertEqual(age, 'age')
 
-        # No first name = invalid 
-        profile.last_name = "Doe" 
-        profile.age = 25
-        profile.gender = "Male"
-        profile.sexuality = "Straight",
-        profile.country = "USA"
-        profile.state_region = "Tenneesee"
-        profile.city = "Nashville"
-        profile.occupation = "Software Engineer"
-        profile.company = "Amazon"
-        profile.education = "Vanderbilt" 
-        profile.bio = "Hello!"
-        profile.religion = "Coding"
-        profile.hobbies = "Coding, not sleeping"
-        profile.dietary_preferences = "Everything"
-        profile.alcohol = True 
-        profile.smoking = False 
-        profile.email = "johndoe@vanderbilt.edu"
-        profile.save()
-
-        record = ProfileModel.objects.get(pk=2)
-        #self.assertNotEqual(record, profile)
-        print(record)
-        #self.assertEqual(record, None)
+    '''def test_createInvalidProfile(self):
+        response = self.client.post() #EEEEEEEEEEEE D: 
+        self.assertEqual(response.status_code, 200)
         self.assetFormError()
-
-    '''def test_checkName(self):
-        # If the profile has been added to the database,
-        # we should be able to print the name of the User 
-        print("Test Check Name")
-        john_profile = ProfileModel.objects.all().filter(first_name="John")
-        self.assertEqual(john_profile.first_name, "John")
-        self.assertEqual(john_profile.last_name, "Doe")
-        print("YEET")
 
     def emailProfiles(self):
         pass'''
